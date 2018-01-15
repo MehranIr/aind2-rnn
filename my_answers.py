d28807b4cb2f2edc400cc73dd7bdbf077785fd38
MehranIr <mehran.firoozbakhtan@gmail.com>
2018-01-15 17:50:56 +0100

    First iteration on the project, finished the financial series and need to still run the text based analysis Signed-off-by: MehranIr <mehran.firoozbakhtan@gmail.com>

--- my_answers.py
+++ my_answers.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import Dense, Activation, LSTM
 import keras
 
 
@@ -12,8 +11,10 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
-
-    # reshape each 
+    for n in range(0, (len(series)-window_size)):
+        X.append(series[n:n+window_size])
+        y.append(series[n+window_size])
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
@@ -23,12 +24,21 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+
+    model = Sequential()
+    model.add(LSTM(5, input_shape= (window_size, 1)))
+    model.add(Dense(1))
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+
 def cleaned_text(text):
+    import string
     punctuation = ['!', ',', '.', ':', ';', '?']
+    for char in text:
+        if char not in punctuation and char not in string.ascii_letters:
+            text = text.replace(char, ' ')
 
     return text
 
@@ -37,10 +47,62 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    index=0
+    while index < (len(text)-window_size):
+        inputs.append(text[index:index+window_size])
+        outputs.append(text[index+window_size])
+        index +=step_size
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars))
+    model.add(Activation('softmax'))
+    return model
+
+
+
+text = ' his eyes she eclipses and predominates the whole of her sex. it was not that he felt any emotion akin to love for irene adler. all emotions, and that one particularly, were abhorrent to his cold, precise but admirably balanced mind. he was, i take it, the most perfect reasoning and observing machine that the world has seen, but as a lover he would have placed himself in a false position. he never spoke of the softer passions, save with a gibe and a sneer. they were admirable things for the observer excellent for drawing the veil from men s motives and actions. but for the trained reasoner to admit such intrusions into his own delicate and finely adjusted temperament was to introduce a distracting factor which might throw a doubt upon all his mental results. grit in a sensitive instrument, or a crack in one of his own high power lenses, would not be more disturbing than a strong emotion in a nature such as his. and yet there was but one woman to him, and that woman was the late irene adler, of dubious and questionable memory. i had seen little of holmes lately. my marriage had drifted us away from each other. my own complete happiness, and the home centred interests which rise up around the man who first finds himself master of his own establishment, were sufficient to absorb all my attention, while holmes, who loathed every form of society with his whole bohemian soul, remained in our lodgings in baker street, buried among his old books, and alternating from week to week between cocaine and ambition, the drowsiness of the drug, and the fierce energy of his own keen nature. he was still, as ever, deeply attracted by the study of crime, and occupied his immense faculties and extraordinary powers of observation in following out those clues, and clearing up those mysteries which had been abandoned as hopeless by the official police. from time to time i heard some vague account of his doings: of his summons to odessa in the case of the trepoff murder, of his clearing up'
+
+    # print out the number of unique characters in the dataset
+chars = sorted(list(set(text)))
+print("this corpus has " + str(len(chars)) + " unique characters")
+print('and these characters are ')
+print(chars)
+# this dictionary is a function mapping each unique character to a unique integer
+chars_to_indices = dict((c, i) for i, c in enumerate(chars))  # map each unique character to unique integer
+
+# this dictionary is a function mapping each unique integer back to a unique character
+indices_to_chars = dict((i, c) for i, c in enumerate(chars))  # map each unique integer back to unique character
+
+
+def encode_io_pairs(text, window_size, step_size):
+    # number of unique chars
+    chars = sorted(list(set(text)))
+    num_chars = len(chars)
+
+    # cut up text into character input/output pairs
+    inputs, outputs = window_transform_text(text, window_size, step_size)
+
+    # create empty vessels for one-hot encoded input/output
+    X = np.zeros((len(inputs), window_size, num_chars), dtype=np.bool)
+    y = np.zeros((len(inputs), num_chars), dtype=np.bool)
+
+    # loop over inputs/outputs and transform and store in X/y
+    for i, sentence in enumerate(inputs):
+        for t, char in enumerate(sentence):
+            X[i, t, chars_to_indices[char]] = 1
+        y[i, chars_to_indices[outputs[i]]] = 1
+
+    return X, y
+# # x, y = window_transform_series(np.array([1,3,5,7,9,11,13]), 2)
+# # print (x)
+# # print(y)
+#
+# window_size = 100
+# step_size = 5
+# X,y = encode_io_pairs(text,window_size,step_size)
